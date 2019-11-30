c9e62afe799a6fcf3fc37ce722ab010167dbba33
chai2010 <chaishushan@gmail.com>
2019-11-30 22:09:51 +0800

    增加签名相关API(未实现)

--- sig.go
+++ sig.go
@@ -2,6 +2,20 @@
 
 package ethutil
 
-// 签名
-// 签名的验证
+// 私钥签名
+// 涉及的字符串都是十六进制编码格式
+func Sign(privateKey, digestHash string) (sig string, err error) {
+	panic("todo")
+}
+
+// 公钥验证签名
+// 涉及的字符串都是十六进制编码格式
+func VerifySignature(pubkey, digestHash, signature string) bool {
+	panic("todo")
+}
+
 // 签名导出公钥
+func GetPublicFromSign(signature string) (publicKey string, err error) {
+	// 首先解码出 v/r/s 参数
+	panic("todo")
+}
